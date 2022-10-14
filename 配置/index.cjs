9c722724d9983dbab97ec3aad74740ed9bd1be99
qiuvc <115798291+qiuvc@users.noreply.github.com>
2022-10-14 21:02:52 +0800

    Update and rename config/index.cjs to 配置/index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -8,22 +8,22 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx36c486737864d2e7',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '0773c4883cf349add17aa6aa25317fff',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '湖北',
+  CITY: '黄冈',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '猪猪',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o1kRR6Z-xqBYSYfVQfggxNKJm6gc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '9Mo9OR_Eqsy_53mbhMpUGIrCcEK8PUeGUwRRodEmwmY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
